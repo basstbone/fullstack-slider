a2b52cbda7e150ff16aed8d5871ee4c911a37273
basstbone <soliver5@una.edu>
2026-09-18 10:41:20 -0500

    Update static file serving directory path

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 
-app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../public', 'index.html'))
